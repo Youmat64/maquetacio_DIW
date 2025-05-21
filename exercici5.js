06ab9103afcd8ffc5bb4c8f946fb8654bad98063
Youmat64 <2024_yousef.fennour@gmail.com>
2025-05-21 18:47:09 +0200

    fet ex4

--- exercici5.js
+++ exercici5.js
@@ -1,11 +1,13 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
+  const lista = document.getElementById("lista-navegacio-header");
+
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
+    lista.style.padding = "30px 10px";
   } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
+    lista.style.padding = "80px 10px";
   }
 }
